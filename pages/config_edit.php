0369d05a90ffe05564302884fc5b1c1df8a92e2d
maksun <makgreg@gmail.com>
2024-04-03 12:12:49 +0200

    Update config_edit.php
    Fixed the issue with the "empty" disabled radio button (the settings was not retained) and also add security form validation.

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -1,21 +1,37 @@
 <?php
 // authenticate
+form_security_validate( 'plugin_Cloneproject_config_edit' );
 auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
+
 // Read results
-$f_hierarchy 	= gpc_get_bool( 'clone_hierarchy', ON );
-$f_sub			= gpc_get_bool( 'clone_subprojects', ON );
-$f_cat			= gpc_get_bool( 'clone_categories', ON );
-$f_versions		= gpc_get_bool( 'clone_versions', OFF );
-$f_custom		= gpc_get_bool( 'clone_customfields', ON );
-$f_users		= gpc_get_bool( 'clone_users', ON );
+$f_hierarchy 		= gpc_get_int( 'clone_hierarchy', ON );
+$f_sub			    = gpc_get_int( 'clone_subprojects', ON );
+$f_cat			    = gpc_get_int( 'clone_categories', ON );
+$f_versions		  = gpc_get_int( 'clone_versions', OFF );
+$f_custom		    = gpc_get_int( 'clone_customfields', ON );
+$f_users		    = gpc_get_int( 'clone_users', ON );
 
 // update results
-plugin_config_set( 'clone_hierarchy', $f_hierarchy );
-plugin_config_set( 'clone_subprojects' , $f_sub );
-plugin_config_set( 'clone_categories', $f_cat );
-plugin_config_set( 'clone_versions', $f_versions );
-plugin_config_set( 'clone_customfields', $f_custom );
-plugin_config_set( 'clone_users', $f_users );
+if( plugin_config_get( 'clone_hierarchy' ) != $f_hierarchy ) {
+	plugin_config_set( 'clone_hierarchy', $f_hierarchy );
+}
+if( plugin_config_get( 'clone_subprojects' ) != $f_sub ) {
+	plugin_config_set( 'clone_subprojects', $f_sub );
+}
+if ( plugin_config_get( 'clone_categories' ) != $f_cat ) {
+	plugin_config_set( 'clone_categories', $f_cat );
+}
+if ( plugin_config_get( 'clone_versions' ) != $f_versions ) {
+	plugin_config_set( 'clone_versions', $f_versions );
+}
+if ( plugin_config_get( 'clone_customfields' ) != $f_custom ) {
+	plugin_config_set( 'clone_customfields', $f_custom );
+}
+if ( plugin_config_get( 'clone_users' ) != $f_users ) {
+	plugin_config_set( 'clone_users', $f_users );
+}
+
 // redirect
-print_header_redirect( plugin_page( 'config',TRUE ) );
+form_security_purge( 'plugin_Cloneproject_config_edit' );
+print_header_redirect( plugin_page( 'config', TRUE ) );
